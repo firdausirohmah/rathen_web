0104b439840f1fa565c03c10163ce73d951daf37
aan-andika99 <aanandika74@gmail.com>
2023-11-18 19:39:25 +0700

    about us

--- resources/views/landing_page/about.blade.php
+++ resources/views/landing_page/about.blade.php
@@ -22,19 +22,10 @@
         </div>
     </section>
     
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Automatic Carousel</title>
-</head>
-<body>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                <div id="carouselExample" class="carousel slide w-100" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="asset/images/swiss.jpg" class="d-block w-100" alt="swiss">
@@ -44,7 +35,7 @@
                         </div>
                         <div class="carousel-item">
                             <!-- Gunakan tag video untuk video -->
-                            <video class="d-block w-100" controls autoplay>
+                            <video id="video1" class="d-block w-50" controls autoplay>
                                 <!-- Sediakan sumber video -->
                                 <source src="asset/images/video1.mp4" type="video/mp4">
                                 Your browser does not support the video tag.
@@ -52,7 +43,7 @@
                         </div>
                         <div class="carousel-item">
                             <!-- Gunakan tag video untuk video -->
-                            <video class="d-block w-100" controls autoplay>
+                            <video id="video2" class="d-block w-50" controls autoplay>
                                 <!-- Sediakan sumber video -->
                                 <source src="asset/images/video2.mp4" type="video/mp4">
                                 Your browser does not support the video tag.
@@ -69,21 +60,85 @@
                     </button>
                 </div>
             </div>
+            <div class="col-lg-6">
+                <p>RATHEN merupakan sportwears brand yang berasal dari Bogor, Indonesia. Memulai industri sportwears pada tahun 2014 dengan berfokus pada pembuatan custom jersey sepakbola/futsal. Berbagai jenis tim, baik dari level korporasi, akademisi, komunitas sepakbola, hingga tim liga futsal profesional telah menjadi bagian perjalanan manis rathen di dunia industri sportwear.</p>
+                <p>Hingga akhirnya, Pada tahun 2016, RATHEN dipercaya menjadi penyedia jersey latihan tim nasional Futsal Indonesia. 
+                </p>
+                <p>Tahun 2020 menjadi langkah awal RATHEN menembus pasar Internasional. Dimulai dari negara di kawasan Asia seperti Timor Leste, bergerak perlahan ke Malaysia, hingga akhirnya Korea Selatan.</p>
+                <p>Tidak hanya puas di level Asia, Rathen melanjutkan langkah awal penetrasi ke pasar Eropa melalui negara Swiss hingga dapat memperluas jangkauan ke benua Amerika, tepatnya negara USA. </p>
+                <p>Pasar internasional memiliki standar mutu produk yang tinggi.
+                    Kami, RATHEN Indonesia, dengan bangga dapat memenuhi persyaratan standar mutu tersebut.
+                    </p>
+            </div>
         </div>
-    </div> 
+    </div>
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     
     <script>
-        // Tambahkan script untuk mengaktifkan perpindahan otomatisa
         $(document).ready(function () {
+            // Inisialisasi carousel dengan interval
             $('#carouselExample').carousel({
                 interval: 2000
             });
+    
+            // Menangani peristiwa slid.bs.carousel
+            $('#carouselExample').on('slid.bs.carousel', function (e) {
+                // Menghentikan pemutaran otomatis video1 dan video2
+                $('#video1')[0].pause();
+                $('#video2')[0].pause();
+    
+                // Memulai pemutaran video pada slide yang aktif
+                var activeIndex = $(this).find('.carousel-item.active').index();
+                if (activeIndex === 2) { // Jika slide ketiga (indeks dimulai dari 0)
+                    $('#video1')[0].play();
+                } else if (activeIndex === 3) { // Jika slide keempat
+                    $('#video2')[0].play();
+                }
+            });
         });
-    </script> --}}
+    </script>
+
+<script>
+    $(document).ready(function () {
+        // Inisialisasi carousel dengan interval
+        $('#carouselExample').carousel({
+            interval: 2000
+        });
+
+        // Menangani peristiwa slid.bs.carousel
+        $('#carouselExample').on('slid.bs.carousel', function (e) {
+            // Menghentikan pemutaran otomatis video1 dan video2
+            $('#video1')[0].pause();
+            $('#video2')[0].pause();
+
+            // Memulai pemutaran video pada slide yang aktif
+            var activeIndex = $(this).find('.carousel-item.active').index();
+            if (activeIndex === 2) { // Jika slide ketiga (indeks dimulai dari 0)
+                $('#video1')[0].play();
+            } else if (activeIndex === 3) { // Jika slide keempat
+                $('#video2')[0].play();
+            }
+        });
+
+        // Menangani interaksi pengguna untuk memulai autoplay
+        $('#startAutoplay').on('click', function () {
+            $('#video1')[0].play();
+            $(this).addClass('d-none'); // Menyembunyikan tombol setelah diklik
+        });
+
+        // Menambahkan logika untuk menunjukkan tombol setelah slide pertama
+        $('#carouselExample').on('slid.bs.carousel', function () {
+            var activeIndex = $(this).find('.carousel-item.active').index();
+            if (activeIndex === 0) {
+                $('#startAutoplay').removeClass('d-none');
+            }
+        });
+    });
+</script>    
+
     
     
 </body>

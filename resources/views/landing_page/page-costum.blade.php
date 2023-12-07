21abdd5589822772c885c9ec86f8c0849286f414
aan-andika99 <aanandika74@gmail.com>
2023-12-07 08:31:37 +0700

    kk

--- resources/views/landing_page/page-costum.blade.php
+++ resources/views/landing_page/page-costum.blade.php
@@ -16,6 +16,7 @@
                         font-size: 8px;
                     }
                     .teks {
+                        /* margin-top: 4px; */
                         font-size: 12px;
                         margin-top: 50px;
                     }
@@ -107,7 +108,7 @@
                         margin-top: 115px;
                     }
                 }
-                @media (max-width: 405px) {
+                @media (max-width: 430px) {
                     .ip {
                         font-size: 8px;
                     }
@@ -116,7 +117,7 @@
                         margin-top: 120px;
                     }
                 }
-                @media (max-width: 410px) {
+                @media (max-width: 460px) {
                     .ip {
                         font-size: 8px;
                     }
@@ -125,6 +126,23 @@
                         margin-top: 125px;
                     }
                 }
+                @media (max-width: 575.98px) { 
+                    .teks {
+                        margin-top: 50%;
+                    }
+                 }
+                @media (max-width: 767.98px) { 
+                    .teks {
+                        margin-top: 50%;
+                 }
+                }
+                @media (max-width: 991.98px) { 
+                    .teks {
+                        margin-top: 62%;
+                 }
+                }
+                /* @media (max-width: 1199.98px) { ... }
+                @media (max-width: 1399.98px) { ... } */
             </style>
             
         <div class="main-banner__cekresi">
@@ -231,7 +249,7 @@
                 </div>
             </div>
             <div class="col-lg-6 about teks" style="font-family: Montheavy; color: rgba(0, 0, 0, 0.8);">
-                <p class=" text-justify mb-4 " >RATHEN merupakan sportwears brand yang berasal dari Bogor, Indonesia. Memulai industri sportwears pada tahun 2014 dengan berfokus pada pembuatan custom jersey sepakbola atau futsal. Berbagai jenis tim, baik dari level korporasi, akademisi, komunitas sepakbola, hingga tim liga futsal profesional telah menjadi bagian perjalanan manis rathen di dunia industri sportwear.</p>
+                <p class=" text-justify mb-4" >RATHEN merupakan sportwears brand yang berasal dari Bogor, Indonesia. Memulai industri sportwears pada tahun 2014 dengan berfokus pada pembuatan custom jersey sepakbola atau futsal. Berbagai jenis tim, baik dari level korporasi, akademisi, komunitas sepakbola, hingga tim liga futsal profesional telah menjadi bagian perjalanan manis rathen di dunia industri sportwear.</p>
                 <p class=" text-justify mb-4">Hingga akhirnya, Pada tahun 2016, RATHEN dipercaya menjadi penyedia jersey latihan tim nasional Futsal Indonesia. 
                 </p>
                 <p class=" text-justify mb-4">Tahun 2020 menjadi langkah awal RATHEN menembus pasar Internasional. Dimulai dari negara di kawasan Asia seperti Timor Leste, bergerak perlahan ke Malaysia, hingga akhirnya Korea Selatan.</p>

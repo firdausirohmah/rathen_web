57707d723d045c6bbb10bc390d4383d56185661d
lsusi3711@gmail.com <Susilchan>
2023-12-17 19:56:03 +0700

    modified:   resources/views/auth/dashboard.blade.php

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,32 +1,4 @@
 @extends('layout.admin')
-<<<<<<< HEAD
-@section('content')
-   <a href="{{ route('downloadPdf') }}"><button>PDF</button></a>
-    <!-- End Navbar -->
-    <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-          <div class="card">
-            <div class="card-body p-3">
-              <div class="row">
-                <div class="col-8">
-                  <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
-                    <h5 class="font-weight-bolder mb-0">
-                      $53,000
-                      <span class="text-success text-sm font-weight-bolder">+55%</span>
-                    </h5>
-                  </div>
-                </div>
-                <div class="col-4 text-end">
-                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-=======
 @section('content')   
 <div class="container">
   <div class="row mt-4">
@@ -38,7 +10,6 @@
             {{-- <i class="fa fa-arrow-up text-success"></i> --}}
             {{-- <span class="font-weight-bold">4% more</span> in 2021 --}}
           </p>
->>>>>>> a43113f02e3fe058e277cc31ea3fcd43941308db
         </div>
         <div class="card-body p-3">
           <div class="chart">
